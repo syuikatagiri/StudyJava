b2a95a3ebb9e93ea6918d3d3e53c13d864060b4d
shui <syui56@icloud.com>
2023-05-07 13:02:07 +0900

    closes #1

--- src/drill1_1/UseRecord.java
+++
@@ -1,9 +0,0 @@
-package drill1_1;
-
-public class UseRecord {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-        System.out.println("test");
-    }
-}
